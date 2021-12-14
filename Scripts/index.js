8acc089effd322f69923164af06d79bccbe2c2a7
Vladimir <vatsup@mail.ru>
2021-12-14 18:47:12 +0300

    First commit

---
+++ Scripts/index.js
@@ -0,0 +1,13 @@
+/* 
+    Тренировочный скрипт для начального курса BeOnMax
+*/
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[0]);
+alert('Hello!');
+let answer = confirm('Are you hear?');
+console.log(answer);
+answer = prompt('Are you hear?');
+console.log(answer);
+answer = prompt('Are you hear?','Yes');
+console.log(answer);
+console.log(typeof(answer));
